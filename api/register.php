e08d51ab5c10730769bd962e2ada67787f2260fa
yagolyagol <154974423+yagolyagol@users.noreply.github.com>
2025-06-30 23:22:03 +0545

    whole code changed in php

--- api/register.php
+++ api/register.php
@@ -56,44 +56,4 @@ if (move_uploaded_file($tmp_name, $upload_path)) {
 }
 ?>
 
-/*include("connect.php");
-$name = $_POST['name'];
-$mobile = $_POST['mobile'];
-$password = $_POST['password'];
-$cpassword = $_POST['cpassword'];
-$address = $_POST['address'];
-$image = $_FILES['Photo']['name'];
-$tmp_name =$_FILES['Photo']['tmp_name'];
-$role = $_POST['role'];
 
-if ($password==$cpassword){//( is_uploaded_file($tmp_name)){
-    move_uploaded_file($tmp_name, "../uploads/$image");
-     $insert = mysqli_query($connect, "INSERT INTO user (name, mobile, password, address, Photo, role, Status, votes) 
-     VALUES('$name', '$mobile','$password','$address','$image', '$role', 0, 0)");   
-     if($insert){
-        echo '
-           <script>
-              alert("Registration Successful!!");
-              window.location = "../";
-            </script>
-        ';
-    }
-    else{
-        echo '
-            <script>
-                alert("Error in Registration");
-                 window.location = "../routes/register.html";
-             </script>
-        ';
-    }
-}
-else{
-    echo '
-        <script>
-            alert("Password did not match!!");
-             window.location = "../routes/register.html";
-        </script>
-    ';
-   
-}
-?>*/
