78fd83af40daa15dfb18e9aed1affc94955a9dba
yagolyagol <154974423+yagolyagol@users.noreply.github.com>
2025-07-12 13:11:52 +0545

    mero wala dashboard

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,144 +1,114 @@
 <?php
-echo '<pre>';
-print_r($_SESSION);
-echo '</pre>';
 session_start();
-if(!isset($_SESSION['userdata'])){
-    header("location: ../");
+
+if (!isset($_SESSION['userdata'])) {
+    header("Location: ../index.html"); // redirect to login if session is not set
+    exit;
 }
 
 $userdata = $_SESSION['userdata'];
 $groupsdata = $_SESSION['groupsdata'];
-if($_SESSION['$userdata']['status'] == 0){
-    $status = '<b style ="color:red">Not Voted</b>';
-}
-else{
-    $status = '<b style = "color:green">Voted</b>';
-}
 
+$status = ($userdata['status'] == 0)
+    ? '<b style="color:red">Not Voted</b>'
+    : '<b style="color:green">Voted</b>';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IntegrityPolls -Dashboard</title>
+    <title>Integrity Polls - Dashboard</title>
     <link rel="stylesheet" href="../css/stylesheet.css">
-</head>
-<body>
     <style>
-        #backbutton {
+        /* Styles moved inline to simplify */
+        #backbutton, #logoutbutton {
             padding: 10px;
             border-radius: 15px;
             width: 10%;
             background-color: rebeccapurple;
             color: blanchedalmond;
-            float: left;
-            margin: 10px;
-            
-        }
-        #logoutbutton{
-             padding: 10px;
-            border-radius: 15px;
-            width: 10%;
-            background-color: rebeccapurple;
-            color: blanchedalmond;
-            float: right;
             margin: 10px;
         }
-        #Profile{
+        #backbutton { float: left; }
+        #logoutbutton { float: right; }
+        #Profile {
             background-color: white;
             width: 30%;
             padding: 20px;
             float: left;
         }
-        #Group{
+        #Group {
             background-color: white;
             width: 60%;
             padding: 20px;
             float: right;
         }
-         #votebtn{
+        #votebtn, #voted {
             padding: 10px;
             border-radius: 15px;
             width: 20%;
-            background-color: rebeccapurple;
             color: blanchedalmond;
             float: right;
         }
-        #mainpanel{
+        #votebtn { background-color: rebeccapurple; }
+        #voted { background-color: green; }
+        #mainpanel {
             padding: 10px;
         }
-        #voted{
-            padding: 10px;
-            border-radius: 15px;
-            width: 20%;
-            background-color: green;
-            color: blanchedalmond;
-            float: right;
-        }
-
-
     </style>
+</head>
+<body>
     <div id="mainSection">
-         <center> 
-    <div id="headerSection">
-    <a href="../"><button id="backbutton">Back</button>></a>
-    <a href="logout.php"><button id="logoutbutton"> Logout</button></a>
-    <h1>Integrity Polls</h1>
-        </div>
-         </center>
-         <hr>
-    <div id="mainpanel">      
-    <div id="Profile"></div>
-     <center>
-        <img src="../uploads/<?php echo $userdata['profile_image']; ?>" width= "100"  height="100">
-     </center><br><br>
-        <b>Name:</b><?php echo $userdata['name']?><br><br>
-        <b>Mobile:</b><?php echo $userdata['mobile']?><br><br>
-        <b>Address:</b><?php echo $userdata['address']?><br><br>
-        <b>Status:</b><?php echo $userdata['status']?><br><br>
-     </div>
+        <center>
+            <div id="headerSection">
+                <a href="../"><button id="backbutton">Back</button></a>
+                <a href="logout.php"><button id="logoutbutton">Logout</button></a>
+                <h1>Integrity Polls</h1>
+            </div>
+        </center>
+        <hr>
+        <div id="mainpanel">
+            <div id="Profile">
+                <center>
+                    <img src="../uploads/<?php echo $userdata['profile_image']; ?>" width="100" height="100">
+                </center><br><br>
+                <b>Name:</b> <?php echo $userdata['name']; ?><br><br>
+                <b>Mobile:</b> <?php echo $userdata['mobile']; ?><br><br>
+                <b>Address:</b> <?php echo $userdata['address']; ?><br><br>
+                <b>Status:</b> <?php echo $status; ?><br><br>
+            </div>
 
-    <div id="Group"></div>
-      <?php
-            if($_SESSION['groupsdata']){
-               for($i=0; $i<count($groupsdata); $i++){
-                 ?>
-                <div>
-                  <img style="float: right" src="../uploads/<?php echo $groupsdata[$i]['photo'] ?>" height="100" width="100">
-                   <b>Group Name: </b><?php echo $groupsdata[$i]['name'] ?><br><br>
-                   <b>Votes: </b><?php echo $groupsdata[$i]['votes'] ?><br><br>
-                   <form action="../api/vote.php" >
-                       <input type="hidden" name="gvotes" value="<?php echo $groupsdata[$i]['votes'] ?>">
-                       <input type="hidden" name ="gid" value="<?php echo $groupsdata[$i]['id'] ?>">
-                       <?php
-                           if($_SESSION['userdata']['status'] == 0){
-                            ?>
-                             <input type="submit" name="votebtn" value="Vote" id="votebtn">
-                            <?php
-
-                           }
-                           else{
-                             ?>
-                             <button disabled type="button" name="votebtn" value="Vote" id="voted"> Voted</button>
-                              <?php
-                           }
+            <div id="Group">
+                <?php
+                if (!empty($groupsdata)) {
+                    foreach ($groupsdata as $group) {
                         ?>
-                   </form>                   
-                </div>
-                 <hr>
-                 <?php
-              }
-         }
-          else{
-         }
-          ?>
-      </div>
+                        <div>
+                            <img style="float: right" src="../uploads/<?php echo $group['photo']; ?>" height="100" width="100">
+                            <b>Group Name:</b> <?php echo $group['name']; ?><br><br>
+                            <b>Votes:</b> <?php echo $group['votes']; ?><br><br>
+                            <form action="../api/vote.php" method="post">
+                                <input type="hidden" name="gvotes" value="<?php echo $group['votes']; ?>">
+                                <input type="hidden" name="gid" value="<?php echo $group['id']; ?>">
+                                <?php if ($userdata['status'] == 0) { ?>
+                                    <input type="submit" name="votebtn" value="Vote" id="votebtn">
+                                <?php } else { ?>
+                                    <button disabled type="button" id="voted">Voted</button>
+                                <?php } ?>
+                            </form>
+                        </div>
+                        <hr>
+                        <?php
+                    }
+                } else {
+                    echo "<p>No candidate groups found.</p>";
+                }
+                ?>
+            </div>
+        </div>
     </div>
- </div>
-    
-
-    
 </body>
 </html>
+
+
