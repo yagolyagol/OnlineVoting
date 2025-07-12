4537a86efd275bd46802b0c3a6edc274ac06f1b8
yagolyagol <154974423+yagolyagol@users.noreply.github.com>
2025-07-12 13:19:36 +0545

    login, new front ,login.php,vote.php,stylescss,register added

--- api/login.php
+++ api/login.php
@@ -1,17 +1,41 @@
- <?php
- include("connect.php");
+<?php
+session_start();
+include("connect.php");
 
- $mobile = $_POST['mobile'];
- $password = $_POST['password'];
- $role = $_POST['role'];
+$mobile = mysqli_real_escape_string($connect, $_POST['mobile']);
+$password = $_POST['password'];
+$role = mysqli_real_escape_string($connect, $_POST['role']);
+
+// Fetch user by mobile and role
+$query = "SELECT * FROM user WHERE mobile='$mobile' AND role='$role'";
+$result = mysqli_query($connect, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    $userdata = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    // If password is plain text
+    if ($userdata['password'] === $password) {
+
+        // Store session
+        $_SESSION['userdata'] = $userdata;
+
+        // Fetch all candidates
+        $groupsQuery = mysqli_query($connect, "SELECT * FROM user WHERE role='candidate'");
+        $_SESSION['groupsdata'] = mysqli_fetch_all($groupsQuery, MYSQLI_ASSOC);
+
+        // Redirect to dashboard
+        header("Location: ../routes/dashboard.php");
+        exit;
+
+    } else {
+        echo "<script>alert('Incorrect password'); window.location='../index.html';</script>";
+    }
+} else {
+    echo "<script>alert('User not found'); window.location='../index.html';</script>";
+}
+?>
+
+
+
+
  
- $check = mysqli_query($connect,"SELECT * FROM user WHERE mobile='$mobile' AND password = '$password' AND role ='$role' ");
-
- if( mysqli_num_rows($check)>0){
-    $userdata=
-
- }
- else{
-      echo "<script>alert('imnvalid credentuals or user mot foumd'); window.location='../routes/register.html';</script>";
- }
- ?>
--- api/vote.php
+++ api/vote.php
@@ -3,30 +3,37 @@ session_start();
 include('connect.php');
 
 $votes = $_POST['gvotes'];
-$total_votes = $votes+1;
 $gid = $_POST['gid'];
 $uid = $_SESSION['userdata']['id'];
 
-$update_votes = mysqli_query($connect, "UPDATE user SET votes ='$total_votes' WHERE id = '$gid' ");
-$update_user_status = mysqli_query($connect, "UPDATE user SET status-1 WHERE id = '$uid' ");
+$total_votes = $votes + 1;
 
-if($update_votes and $update_user_status){
-     $group = mysqli_fetch_array($connect, "SELECT * FROM user WHERE role =2");
-    $groupsdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);//groupdata fetch
-    $_SESSION['userdata']['status']= 1;
-    $_SESSION['groupsdata'] = $groupsdata;
-    echo'
+// Update votes for the selected candidate/group
+$update_votes = mysqli_query($connect, "UPDATE user SET votes = '$total_votes' WHERE id = '$gid'");
+
+// Update voter status to mark as "voted"
+$update_user_status = mysqli_query($connect, "UPDATE user SET status = 1 WHERE id = '$uid'");
+
+if ($update_votes && $update_user_status) {
+    // Refresh session data
+    $userdata_query = mysqli_query($connect, "SELECT * FROM user WHERE id = '$uid'");
+    $_SESSION['userdata'] = mysqli_fetch_array($userdata_query, MYSQLI_ASSOC);
+
+    $groups_query = mysqli_query($connect, "SELECT * FROM user WHERE role = 'candidate'");
+    $_SESSION['groupsdata'] = mysqli_fetch_all($groups_query, MYSQLI_ASSOC);
+
+    echo '
     <script>
         alert("Voting Successful!!");
-             window.location="../routes/dashboard.php";
+        window.location = "../routes/dashboard.php";
     </script>';
-
-} 
-else{
+} else {
     echo '
-     <script>
-        alert("Error Occured");
-             window.location="../routes/dashboard.php";
+    <script>
+        alert("Error Occurred While Voting!");
+        window.location = "../routes/dashboard.php";
     </script>';
 }
 ?>
+
+
